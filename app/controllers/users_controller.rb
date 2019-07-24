e968ec36fb80cfb5e4d16eff78b511a1b7ced1d4
EC2 Default User <ec2-user@ip-172-31-22-241.us-east-2.compute.internal>
2019-07-24 08:14:59 +0000

    task

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(name: params[:user][:name], address:[:user][:address],
+    email: [:user][:email], password: [:user][:password])
+      if @user.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+  
+end
+
