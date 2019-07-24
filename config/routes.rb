d754a7e249078aa614b2ff53363209694eaa9a92
exq-sub <ec2-user@ip-172-31-22-241.us-east-2.compute.internal>
2019-07-24 16:39:14 +0000

    task3

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   
   resources :users
   
+  get 'logout' => 'sessions#destroy'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  
   
 end
